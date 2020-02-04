0673b1091e55ec58d0f503c4cd09cbc1686ad701
Farquaadglob <44657957+Farquaadglob@users.noreply.github.com>
2020-02-04 12:54:30 +0800

    register and login
    ive added the registration for org and public users together with the login

---
+++ Shimmer/BLL/Account.cs
@@ -0,0 +1,46 @@
+﻿using Shimmer.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shimmer.BLL
+{
+    public class Account
+    {
+        //Define class properties
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string UserType { get; set; }
+
+        //Default contructor
+        public Account()
+        {
+
+        }
+        //Define a constructor to initialize all the properties
+        public Account(string fullname, string email, string password, string phoneno, string usertype)
+        {
+            FullName = fullname;
+            Email = email;
+            Password = password;
+            Phone = phoneno;
+            UserType = usertype;
+        }
+
+        public int AddAccount()
+        {
+            AccountDAO dao = new AccountDAO();
+            int result = dao.Insert(this);
+            return result;
+        }
+
+        public string CheckPassword(string password)
+        {
+            AccountDAO dao = new AccountDAO();
+            return dao.CheckPassword(password);
+        }
+    }
+}
---
+++ Shimmer/DAL/AccountDAO.cs
@@ -0,0 +1,86 @@
+﻿using Shimmer.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Shimmer.DAL
+{
+    public class AccountDAO
+    {
+        public int Insert(Account acc)
+        {
+            // Execute NonQuery return an integer value
+            int result = 0;
+            SqlCommand sqlCmd = new SqlCommand();
+
+            //Step 1 -  Define a connection to the database by getting
+            //          the connection string from web.config
+            string DBConnect = ConfigurationManager.ConnectionStrings["ShimmerConnectionString"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            // Step 2 - Instantiate SqlCommand instance to add record 
+            //          with INSERT statement
+            string sqlStmt = "INSERT INTO Account (fullname, email, password, phoneno, usertype) " +
+                "VALUES (@paraFullName, @paraEmail, @paraPassword, @paraPhone, @paraUserType)";
+            sqlCmd = new SqlCommand(sqlStmt, myConn);
+
+            // Step 3 : Add each parameterised variable with value
+            sqlCmd.Parameters.AddWithValue("@paraFullName", acc.FullName);
+            sqlCmd.Parameters.AddWithValue("@paraEmail", acc.Email);
+            sqlCmd.Parameters.AddWithValue("@paraPassword", acc.Password);
+            sqlCmd.Parameters.AddWithValue("@paraPhone", (object)acc.Phone ?? DBNull.Value);
+            sqlCmd.Parameters.AddWithValue("@paraUserType", acc.UserType);
+
+
+
+            // Step 4 Open connection the execute NonQuery of sql command   
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            // Step 5 :Close connection
+            myConn.Close();
+
+            return result;
+        }
+
+        public string CheckPassword(string email)
+        {
+            //Step 1 -  Define a connection to the database by getting
+            //          the connection string from web.config
+            string DBConnect = ConfigurationManager.ConnectionStrings["ShimmerConnectionString"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            //Step 2 -  Create a DataAdapter to retrieve data from the database table
+            string sqlStmt = "Select password from dbo.[Account] where email = @paraEmail";
+            SqlDataAdapter da = new SqlDataAdapter(sqlStmt, myConn);
+
+            da.SelectCommand.Parameters.AddWithValue("@paraEmail", email);
+
+            //Step 3 -  Create a DataSet to store the data to be retrieved
+            DataSet ds = new DataSet();
+
+            //Step 4 -  Use the DataAdapter to fill the DataSet with data retrieved
+            da.Fill(ds);
+
+            //Step 5 -  Read data from DataSet.
+            string acctPass = null;
+            int rec_cnt = ds.Tables[0].Rows.Count;
+            if (rec_cnt == 1)
+            {
+                DataRow row = ds.Tables[0].Rows[0];  // Sql command returns only one record
+                string password = row["password"].ToString();
+                acctPass = password;
+            }
+            else
+            {
+                acctPass = null;
+            }
+
+            return acctPass;
+        }
+    }
+}
